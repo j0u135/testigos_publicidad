a93c931796e1b7dad35c9757f26ddc65d108ba8a
j0u135 <j0u135.uam@gmail.com>
2015-08-02 04:45:07 +0000

    Crea rutas, controlador y modelo de Agency

---
+++ app/controllers/agencies_controller.rb
@@ -0,0 +1,25 @@
+class AgenciesController < ApplicationController
+    
+    def index
+    end
+    
+    def new
+    end
+    
+    def create
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def show
+    end
+    
+    private
+        def agency_params.require(:agency_name)
+        end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   resources :orders
   resources :creatives
+  resources :agencies
 
 end
