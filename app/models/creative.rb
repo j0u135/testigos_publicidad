06bbc4a62e47b85b70589dc9a1f3d4f6d3d27d71
j0u135 <j0u135.uam@gmail.com>
2015-08-07 02:47:47 +0000

    corrige typo en el modelo de Creative en la relación con lines y pages

--- app/models/creative.rb
+++ app/models/creative.rb
@@ -1,7 +1,7 @@
 class Creative < ActiveRecord::Base
     belongs_to :order
     has_many :pages
-    has_many :lines :through :pages
+    has_many :lines, through: :pages
     
     validates :creative_name, presence: true, length: { minimum: 2, maximum: 50 }
 end
